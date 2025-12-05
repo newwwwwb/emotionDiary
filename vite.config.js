010a13140d8778fea8dfa6c177f69f0e45fe51b0
Tae <a91639754@gmail.com>
2025-12-05 16:45:08 +0900

    세번째 배포

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig(({mode}) => ({
-  base: mode === "production" ? "/emotiondiary/" : "/",
+  base: mode === "production" ? "/emotionDiary/" : "/",
   build: {
     outDir: "docs",
   },
